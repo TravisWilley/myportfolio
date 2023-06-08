94239261412f07e75c92a3dff76d485942897918
Travis <travis@travisww.com>
2023-06-08 08:57:15 -0700

    about

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -3,9 +3,9 @@ import { TbBooks } from 'react-icons/tb';
 import { BiHappyHeartEyes } from 'react-icons/bi';
 
 const data = [
-  { id: 1, icon: <FaAward />, title: 'Experience', desc: '1+ years' },
-  { id: 2, icon: <TbBooks />, title: 'Projects', desc: '' },
-  { id: 3, icon: <BiHappyHeartEyes />, title: 'Clients', desc: '' },
+  { id: 1, icon: <FaAward />, title: 'Experience', desc: '2+ years' },
+  { id: 2, icon: <TbBooks />, title: 'Projects', desc: '2' },
+  { id: 3, icon: <BiHappyHeartEyes />, title: 'Clients', desc: '1' },
 ];
 
 export default data;
